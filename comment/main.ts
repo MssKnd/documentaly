77e36e3b395339d6ec4d721c86efeda7f75b1cc2
Masashi Kondo <45549311+MssKnd@users.noreply.github.com>
2023-01-02 07:55:19 +0900

    Feature/refactore (#3)
    * refactoring
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- comment/main.ts
+++ comment/main.ts
@@ -1,4 +1,3 @@
-import { parse } from "https://deno.land/std@0.100.0/flags/mod.ts";
 import { validateFilePath } from "../search-markdown-files/file-path.ts";
 import { validateMarkdownFilePath } from "../search-markdown-files/markdown-file-path.ts";
 import { isObject, isString } from "../utilities/type-guard.ts";
@@ -41,26 +40,21 @@ function blobUrlBase(branchName: string, headSha: string) {
   return `https://github.com/${branchName}/blob/${headSha}/`;
 }
 
-function main(json: unknown, branchName: string, headSha: string) {
+function comment(json: string, branchName: string, headSha: string) {
   const dependencyMap = validateDependencyMap(json);
   if (dependencyMap.size === 0) {
-    return "未変更のドキュメントはありません";
+    return "未更新のドキュメントは無いようです 👀";
   }
   const baseBlobUrl = blobUrlBase(branchName, headSha);
-  return Array.from(dependencyMap.entries()).map(
+  const result = Array.from(dependencyMap.entries()).map(
     ([markdownFilePath, filePaths]) => {
-      return `未変更のドキュメント（[${markdownFilePath}](${baseBlobUrl}${markdownFilePath})）に関連している以下のファイルが変更されています。\n${
+      return `未更新のドキュメント（[${markdownFilePath}](${baseBlobUrl}${markdownFilePath})）に関連した以下のファイルが変更されています。ドキュメントの更新は必要ありませんか？\n${
         filePaths.map((filePath) => `- ${filePath}\n`).join("")
       }
     `;
     },
   ).join("\n\n");
+  console.log(result);
 }
 
-const {
-  j: json,
-  s: headSha,
-  b: branchName,
-} = parse(Deno.args);
-
-console.log(main(json, branchName, headSha));
+export { comment };
--- get-configuration/command-line-arguments/argument-config.ts
+++ get-configuration/command-line-arguments/argument-config.ts
@@ -4,18 +4,42 @@ import {
 } from "../../search-markdown-files/file-path.ts";
 import { isBoolean, isObject, isString } from "../../utilities/mod.ts";
 
+type Command = "check" | "comment";
+
 type CommandLineArgument = {
   helpFlag: boolean;
+  command: Command;
   targetBranch: string;
   filePaths: FilePath[];
+  json?: string;
+  headSha?: string;
+  branchName?: string;
 };
+
+function validateCommand(input: unknown): Command {
+  if (!isString(input)) {
+    throw new Error();
+  }
+  switch (input) {
+    case "check":
+      return "check";
+    case "comment":
+      return "comment";
+    default:
+      throw new Error("invalid command");
+  }
+}
 
 /** validate command line argument */
 function validateCommandLineArgument(input: unknown) {
   const baseConfig: CommandLineArgument = {
     helpFlag: false,
     targetBranch: "main",
+    command: "check",
     filePaths: ["." as FilePath],
+    json: undefined,
+    headSha: undefined,
+    branchName: undefined,
   };
   if (!isObject(input)) {
     throw new Error();
@@ -35,12 +59,36 @@ function validateCommandLineArgument(input: unknown) {
     baseConfig.targetBranch = "main";
   }
   if (
-    "_" in input && Array.isArray(input._) && input._.length > 0
+    "j" in input && isString(input.j)
   ) {
-    baseConfig.filePaths = input._.map((filePath) =>
+    baseConfig.json = input.j;
+  } else {
+    baseConfig.json = "[]";
+  }
+  if (
+    "s" in input && isString(input.s)
+  ) {
+    baseConfig.headSha = input.s;
+  } else {
+    baseConfig.headSha = undefined;
+  }
+  if (
+    "b" in input && isString(input.b)
+  ) {
+    baseConfig.branchName = input.b;
+  } else {
+    baseConfig.branchName = undefined;
+  }
+  if (
+    "_" in input && Array.isArray(input._)
+  ) {
+    const [command, ...filePaths] = input._;
+    baseConfig.command = validateCommand(command);
+    baseConfig.filePaths = filePaths.map((filePath) =>
       validateFilePath(filePath)
     );
-  } else {
+  }
+  if (baseConfig.filePaths.length === 0) {
     baseConfig.filePaths = [validateFilePath(".")];
   }
   return baseConfig;
