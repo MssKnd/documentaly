89c291c220fc78e3cc3b98d5d5e07d6eff5dc9b4
GitHub <noreply@github.com>
2023-01-02 14:16:00 +0000

    chore(deps): update deno dependencies

--- check/arrange-git-diff/main.ts
+++ check/arrange-git-diff/main.ts
@@ -1,4 +1,4 @@
-import $ from "https://deno.land/x/dax@0.17.0/mod.ts";
+import $ from "https://deno.land/x/dax@0.20.0/mod.ts";
 import { validateFilePath } from "../search-markdown-files/mod.ts";
 
 function diff(target: string): Promise<string> {
