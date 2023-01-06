90cde3a9f29941098f17704d3cb54f6a1285d6ab
Masashi Kondo <masashi.kondo@rokka.dev>
2023-01-06 09:23:48 +0900

    modnize

--- utilities/command-line-arguments/main.ts
+++
@@ -1,9 +0,0 @@
-import { parse } from "https://deno.land/std@0.170.0/flags/mod.ts";
-import { validateCommandLineArgument } from "./argument-config.ts";
-
-/** parsing and validating command line argument */
-function main() {
-  return validateCommandLineArgument(parse(Deno.args));
-}
-
-export { main as commandLineArgument };
--- utilities/command-line-arguments/mod.ts
+++ utilities/command-line-arguments/mod.ts
@@ -1,1 +1,9 @@
-export { commandLineArgument } from "./main.ts";
+import { parse } from "https://deno.land/std@0.170.0/flags/mod.ts";
+import { validateCommandLineArgument } from "./argument-config.ts";
+
+/** parsing and validating command line argument */
+function main() {
+  return validateCommandLineArgument(parse(Deno.args));
+}
+
+export { main as commandLineArgument };
