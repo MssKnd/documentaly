ed668d1b59b30127a2795b71c8083caa30dd62b8
GitHub <noreply@github.com>
2023-01-09 11:18:12 +0000

    chore(deps): update deno dependencies

--- utilities/command-line-arguments/mod.ts
+++ utilities/command-line-arguments/mod.ts
@@ -1,4 +1,4 @@
-import { parse } from "https://deno.land/std@0.170.0/flags/mod.ts";
+import { parse } from "https://deno.land/std@0.171.0/flags/mod.ts";
 import { validateCommandLineArgument } from "./argument-config.ts";
 
 /** parsing and validating command line argument */
