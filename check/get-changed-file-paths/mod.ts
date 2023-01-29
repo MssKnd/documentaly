abacd6cb327e57eb7f6bbf4f5055728bfaa6fd8d
Masashi Kondo <masashi.kondo@rokka.dev>
2023-01-29 22:22:05 +0900

    fix

--- check/get-changed-file-paths/mod.ts
+++ check/get-changed-file-paths/mod.ts
@@ -5,9 +5,11 @@ function diff(target: string): Promise<string> {
   return $`git diff --name-only HEAD ${target}`.text();
 }
 
-async function main(targetBranchName: string): Promise<FilePath[]> {
+async function getChangedFilePaths(
+  targetBranchName: string,
+): Promise<FilePath[]> {
   const result = await diff(targetBranchName);
   return result.split(/\n/).map((filePath) => validateFilePath(filePath));
 }
 
-export { main as getChangedFiles };
+export { getChangedFilePaths };
--- utilities/extruct-yaml-header/mod.ts
+++ utilities/extruct-yaml-header/mod.ts
@@ -1,8 +1,8 @@
 function extructYamlHeader(markdown: string) {
   const headerRegex = /^---$\n(?<props>[\s\S]*?)\n^---$(?<body>[\s\S]*)/m;
   const matchResult = markdown.match(headerRegex);
-  const yamlHeader = matchResult?.groups?.props ?? ""
-  const body = yamlHeader ? matchResult?.groups?.body?.trim() ?? "" : markdown
+  const yamlHeader = matchResult?.groups?.props ?? "";
+  const body = yamlHeader ? matchResult?.groups?.body?.trim() ?? "" : markdown;
   return {
     yamlHeader,
     body,
