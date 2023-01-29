0adcd0aa3dc92624c6caefa30a57f737bd138a2b
Masashi Kondo <masashi.kondo@rokka.dev>
2023-01-29 22:13:51 +0900

    fix

--- utilities/extruct-yaml-header/mod.ts
+++ utilities/extruct-yaml-header/mod.ts
@@ -1,9 +1,11 @@
 function extructYamlHeader(markdown: string) {
   const headerRegex = /^---$\n(?<props>[\s\S]*?)\n^---$(?<body>[\s\S]*)/m;
   const matchResult = markdown.match(headerRegex);
+  const yamlHeader = matchResult?.groups?.props ?? ""
+  const body = yamlHeader ? matchResult?.groups?.body?.trim() ?? "" : markdown
   return {
-    yamlHeader: matchResult?.groups?.props ?? "",
-    body: matchResult?.groups?.body ?? "",
+    yamlHeader,
+    body,
   };
 }
 
