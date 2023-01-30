a3a837589b642a7ed5d2300372fd22dabf05552b
Masashi Kondo <masashi.kondo@rokka.dev>
2023-01-30 20:49:49 +0900

    add test

---
+++ publish/publish.test.ts
@@ -0,0 +1,24 @@
+import {
+  assertSpyCall,
+  spy,
+} from "https://deno.land/std@0.165.0/testing/mock.ts";
+import { FilePath } from "../check/file-path/mod.ts";
+import { publish } from "./mod.ts";
+
+Deno.test("publish", async (t) => {
+  await t.step("valid: No filePaths input.", () => {
+    // given:
+    const logSpy = spy(console, "info");
+    const filePaths: FilePath[] = [];
+
+    // when:
+    publish({
+      filePaths,
+    });
+
+    // then:
+    assertSpyCall(logSpy, 0, {
+      args: ["No change files."],
+    });
+  });
+});
