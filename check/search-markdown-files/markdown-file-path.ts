635a17d823051d3e3ad4e6db05588d8f64fe92b3
Masashi Kondo <masashi.kondo@rokka.dev>
2023-02-03 21:37:04 +0900

    fix

--- check/search-markdown-files/markdown-file-path.ts
+++ check/search-markdown-files/markdown-file-path.ts
@@ -5,6 +5,7 @@ type MarkdonwFilePath = Opaque<"MarkdonwFilePath">;
 
 function validateMarkdownFilePath(input: unknown) {
   const filePath = validateFilePath(input);
+  console.log('xxxx',filePath)
   if (!hasMarkdownExtention(filePath)) {
     // TODO: will change Result Type
     throw new Error("invalid markdown file path");
