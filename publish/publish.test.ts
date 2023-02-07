06ec3077646348514b2b67c998dafcffaed014fa
Masashi Kondo <masashi.kondo@rokka.dev>
2023-02-07 09:00:56 +0900

    fix test

--- publish/publish.test.ts
+++ publish/publish.test.ts
@@ -2,18 +2,18 @@ import {
   assertSpyCall,
   spy,
 } from "https://deno.land/std@0.176.0/testing/mock.ts";
-import { FilePath } from "../utilities/path/mod.ts";
+import { MarkdonwFilePath } from "../utilities/path/mod.ts";
 import { publish } from "./mod.ts";
 
 Deno.test("publish", async (t) => {
   await t.step("valid: No filePaths input.", () => {
     // given:
     const logSpy = spy(console, "info");
-    const filePaths: FilePath[] = [];
+    const markdownFilePaths: MarkdonwFilePath[] = [];
 
     // when:
     publish({
-      filePaths,
+      markdownFilePaths,
     });
 
     // then:
